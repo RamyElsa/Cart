87a15fd8a222ab4713a9c4b18e7d4afbe8fb7d47
RamyElsa <ramye301@gmail.com>
2023-01-27 17:44:19 +0200

    first commit

---
+++ Cart/Infrastructure/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Cart.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+namespace Cart.Infrastructure
+{
+    public class DataContext : IdentityDbContext<AppUser>
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
